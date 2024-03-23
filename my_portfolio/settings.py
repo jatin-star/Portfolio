81ae51d110eb07ea351752710a8ee2e7cd573076
jatin-star <75898881+jatin-star@users.noreply.github.com>
2024-03-23 22:54:16 +0530

    Update settings.py

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -45,7 +45,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.staticfiles.storage.StaticFilesStorage',
 ]
 
 ROOT_URLCONF = 'my_portfolio.urls'
