014e88fb2ccdfde6d23274c76d3cc90ea02fb521
jatin-star <75898881+jatin-star@users.noreply.github.com>
2024-04-06 13:57:16 +0530

    Update urls.py

--- Hello/urls.py
+++ Hello/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('Hello/', views.Hello, name='Hello'),
-    path('', views.portfolio_page, name='Portfolio')
+    path('', views.portfolio_page, name='Portfolio'),
+    path('mail/', views.contact_view, name='Mail')
 ]
