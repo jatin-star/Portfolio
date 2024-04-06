489f3b48aceb865ae91c890e310e0670eb66c7d3
jatin-star <75898881+jatin-star@users.noreply.github.com>
2024-04-06 13:57:35 +0530

    Update views.py

--- Hello/views.py
+++ Hello/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 def Hello(request):
     return HttpResponse("Hello world!")
 
+
 def portfolio_page(request):
     return render(request, "index.html")
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('Name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        # Send email
+        send_mail(
+            'Contact Info',  # Subject
+            f'Name: {name}\nEmail: {email}\nMessage: {message}',  # Message
+            settings.EMAIL_HOST_USER,  # Sender email
+            ['himanshuprajapati@usf.edu'],  # Recipient email(s)
+            fail_silently=False,  # Set to True to ignore errors
+        )
+
+        return render(request, 'mail.html')
